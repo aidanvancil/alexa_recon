9704cba739f025dea22a149d298fd39d64993146
moreno-moises <moreno_moises@ymail.com>
2023-10-16 12:50:35 -0700

    capturing packets

---
+++ capture_packets.py
@@ -0,0 +1,22 @@
+import pyshark
+
+# Define the interface for capturing WLAN traffic
+interface = 'wlan0'
+
+# Define a capture filter (you may need to adjust this based on your specific requirements)
+capture_filter = 'host <AMAZON_ECHO_IP>'
+
+# Define the file to save the captured packets
+output_file = 'captured_packets.pcap'
+
+# Start capturing packets
+capture = pyshark.LiveCapture(interface=interface, display_filter=capture_filter)
+
+try:
+    # Start capturing and write to file
+    capture.sniff(packet_count=100, timeout=10)
+    capture.close()
+    capture.export_pcap(output_file)
+    print(f'Captured {len(capture)} packets. Saved to {output_file}')
+except Exception as e:
+    print(f'An error occurred: {e}')
