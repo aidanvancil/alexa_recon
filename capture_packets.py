76dabf1af6903ed2d3d74524bcec94a56afb1815
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2023-10-17 11:29:00 -0700

    add: port_scanner
    port scanner information obtained from technical documentation on python-nmap.

--- capture_packets.py
+++ capture_packets.py
@@ -1,4 +1,22 @@
 import pyshark
+import nmap
+
+# Initializing port scanner object
+def port_scan(nm=nmap.PortScanner()):
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (host, nm[host].hostname()))
+        print('State : %s' % nm[host].state())
+        for proto in nm[host].all_protocols():
+            print('----------')
+            print('Protocol : %s' % proto)
+
+            lport = nm[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+port_scan()
 
 # Define the interface for capturing WLAN traffic
 interface = 'ciscodump'
